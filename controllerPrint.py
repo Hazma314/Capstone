79a3ebba467936012c1d540066d3a54ba554fdcd
Jonas Gordon <indyanajonas@gmail.com>
2026-03-26 17:04:46 -0400

    get stick values, save to variables, print

--- controllerPrint.py
+++ controllerPrint.py
@@ -1,25 +1,21 @@
 import pygame
+import time
 
 pygame.init()
 
-def main():
-
-    # This dict can be left as-is, since pygame will generate a
-    # pygame.JOYDEVICEADDED event for every joystick connected
-    # at the start of the program.
-    joysticks = {}
-
+#pygame.joystick.init()
+print(pygame.joystick.get_count())
+try:
     while(1):
-        a0 = joysticks.get_axis(0)
-        a1 = joysticks.get_axis(1)
-        a2 = joysticks.get_axis(2)
-        a3 = joysticks.get_axis(3)
+        pygame.event.get()
+        controller = pygame.joystick.Joystick(0)
+        a0 = controller.get_axis(0)
+        a1 = controller.get_axis(1)
+        a3 = controller.get_axis(3)
+        a4 = controller.get_axis(4)
+        print(f"0: {a0:>6.3f} 1: {a1:>6.3f} 3: {a3:>6.3f} 4: {a4:>6.3f}\n")
+        print("\n")
+        time.sleep(0.1)
 
-        print(f"0: {a0:>6.3f} 1: {a1:>6.3f} 2: {a2:>6.3f} 3: {a3:>6.3f}\n")
-
-
-if __name__ == "__main__":
-    main()
-    # If you forget this line, the program will 'hang'
-    # on exit if running from IDLE.
+except KeyboardInterrupt:
     pygame.quit()
